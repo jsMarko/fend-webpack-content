4e5aaa572d4d08cde0988995b53f74fdfab073d2
Mark Branscum <mbstudios@hotmail.com>
2020-05-01 13:35:29 -0500

    setting up babel ... trouble with rules in webpack.config.js ... working on it

--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,13 @@ const webpack = require("webpack")
 
 module.exports = {
    entry: "./src/client/index.js",
+   module: {
+      rules: [
+         {
+            test: "/.js$/", // cant put '/\.js$/'   \ <---- disappears when I save it
+            exclude: /node_modules/,
+            loader: "babel-loader",
+         },
+      ],
+   },
 }
